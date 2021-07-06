bdd3e8f4445d10018ecd6c8826b4fc234d45a41b
Nima Rahmanian <nimsi@berkeley.edu>
2021-07-06 12:55:19 +0000

    extension fixes

--- src/runner.py
+++ src/runner.py
@@ -24,8 +24,8 @@ def main_go_enrichment():
         cur_module=l.strip()[1:-1].split(", ")
         results=check_group_enrichment(cur_module, background_genes, os.path.join(constants.dir_path,"data"), th=qval_th)
         df_results=pd.DataFrame(data=results)
-        output_go_filename=f'{os.path.join(output_folder,"module_go")}_{i}.txt'
-        output_module_filename=f'{os.path.join(output_folder,"module_genes")}_{i}.tsv'
+        output_go_filename=f'{os.path.join(output_folder,"module_go")}_{i}.tsv'
+        output_module_filename=f'{os.path.join(output_folder,"module_genes")}_{i}.txt'
         df_results.to_csv(output_go_filename, sep='\t',index_label="index")
         open(output_module_filename,'w+').write("\n".join(cur_module))
         print(f'written GO enrichment results to {output_go_filename} and {output_module_filename}')
@@ -49,5 +49,5 @@ def main_visualize_module():
     output_folder = args.output_folder
     module_index = os.path.splitext(os.path.basename(module_file_name))[0].split('_')[-1]
     visualize_module(module_file_name, module_index, active_genes_file_name, network_file_name, go_file_name, output_folder)
-    print(f'finished generating module #{module_index}')
+    print(f'finished generating module ${module_index}')
 
